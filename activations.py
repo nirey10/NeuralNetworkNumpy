fixed gradient and jacobian tests

activations.py
@@ -31,7 +31,9 @@ class Tanh(Abstract_Activation):
         self.type = 'activation'
 
     def forward(self, x: np.ndarray) -> np.ndarray:
-        return np.tanh(x)
+        self.activation_output = np.tanh(x)
+        return self.activation_output
 
     def backward(self, d_y: np.ndarray) -> np.ndarray:
-        return 1.0 - np.tanh(d_y) ** 2
+        self.activation_grad = 1.0 - np.tanh(d_y) ** 2
+        return self.activation_grad

models.py
@@ -27,7 +27,7 @@ class MyNeuralNetwork(Abstract_Model):
                 weights, bias = f.parameters()
                 # weights.data = .01 * np.random.randn(weights.data.shape[0], weights.data.shape[1])
                 weights.data = .01 * np.random.random((weights.data.shape[0], weights.data.shape[1]))
-                bias.data = 0.
+                bias.data = np.zeros((1, weights.data.shape[1]))
 
     def fit(self, X_train, y_train, X_test, y_test, batch_size, num_epochs, optimizer):
         loss_history = []

network_tests.py
@@ -5,8 +5,9 @@ import layers
 import copy
 import utils
 import activations
+from numpy import linalg as LA
 
-def grad_test(X_train, y_train):
+def grad_test_W(X_train, y_train):
     softmax_in = 2
     softmax_out = 5
     model = models.MyNeuralNetwork()
@@ -18,95 +19,24 @@ def grad_test(X_train, y_train):
     eps0 = 1
     eps = np.array([(0.5 ** i) * eps0 for i in range(10)])
 
-    one_d = np.random.rand(X_train.shape[1])
-    #one_d = one_d / np.sum(one_d)
-    #one_d = np.ones(X_train.shape[1])
-    d = np.tile(one_d, (X_train.shape[0], 1))
-
-
-    grad_diff = []
-
-    for epss in eps:
-        X_train_test = X_train + epss * d
-        model_grad = copy.deepcopy(model)
-        probabilities_grad = model_grad.forward(np.array(X_train))
-        model2 = copy.deepcopy(model)
-        probabilities_grad2 = model2.forward(np.array(X_train_test))
-
-        grad_diff.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, y_train) - utils.cross_entropy_loss(probabilities_grad, y_train)))
-
-    fig, axs = plt.subplots(2, 2, figsize=(12, 8), constrained_layout=True)
-    fig.suptitle('Gradient test for softmax by weights', fontsize=16)
-
-    axs[0, 0].plot(eps, grad_diff)
-    axs[0, 0].set_xlabel('$\epsilon$')
-    axs[0, 0].set_title('$|f(x+\epsilon d) - f(x)|$')
-
-    axs[0, 1].plot(range(len(grad_diff) - 1), [grad_diff[i + 1] / grad_diff[i] for i in range(len(grad_diff) - 1)])
-    axs[0, 1].set_xlabel('$i$')
-    axs[0, 1].set_title('rate of decrease')
-    axs[0, 1].set_ylim([0, 1])
-
-    grad_diff = []
-    for epss in eps:
-        X_train_test = X_train + epss * d
-        model_grad = copy.deepcopy(model)
-        probabilities_grad = model_grad.forward(X_train)
-        model2 = copy.deepcopy(model)
-        probabilities_grad2 = copy.deepcopy(model2.forward(X_train_test))
-        model2.backward(y_train)
-        grad_x = model2.graph[0].weights.grad
-        grad_diff.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, y_train) -
-                                utils.cross_entropy_loss(probabilities_grad, y_train) -
-                                epss * sum(np.dot(np.array([one_d]), grad_x).T)))
-    # np.dot(d.T, grad_x)))
-    axs[1, 0].plot(eps, grad_diff)
-    axs[1, 0].set_xlabel('$\epsilon$')
-    axs[1, 0].set_title('$|f(x+\epsilon d) - f(x) - \epsilon d^{T} grad(x)|$')
-
-    axs[1, 1].plot(range(len(grad_diff) - 1), [grad_diff[i + 1] / grad_diff[i] for i in range(len(grad_diff) - 1)])
-    axs[1, 1].set_xlabel('$i$')
-    axs[1, 1].set_title('rate of decrease')
-    axs[1, 1].set_ylim([0, 1])
-
-    plt.show()
-
-
-
-def grad_test2(X_train, y_train):
-    softmax_in = 2
-    softmax_out = 5
-    model = models.MyNeuralNetwork()
-    model.add(layers.Softmax(softmax_in, softmax_out))
-    model.init()
-    for p in model.parameters:
-        p.grad = 0.
-
-    eps0 = 1
-    eps = np.array([(0.5 ** i) * eps0 for i in range(10)])
-
-    #d = np.random.rand()
     d = np.random.random((2, 5))
     d = d / np.sum(d)
-    #one_d = one_d / np.sum(one_d)
-    #one_d = np.ones(X_train.shape[1])
-    #d = np.tile(one_d, (X_train.shape[0], 1))
-
-
     grad_diff = []
+
+    x_data = np.array([X_train[0]])
+    x_label = np.array([y_train[0]])
 
     for epss in eps:
         model_grad = copy.deepcopy(model)
-        probabilities_grad = model_grad.forward(np.array(X_train))
+        probabilities_grad = model_grad.forward(x_data)
         model2 = copy.deepcopy(model)
         model2.graph[0].weights.data += d*epss
-        probabilities_grad2 = model2.forward(np.array(X_train))
-
-        grad_diff.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, y_train) -
-                                utils.cross_entropy_loss(probabilities_grad, y_train)))
+        probabilities_grad2 = model2.forward(x_data)
+        grad_diff.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, x_label) -
+                                utils.cross_entropy_loss(probabilities_grad, x_label)))
 
     fig, axs = plt.subplots(2, 2, figsize=(12, 8), constrained_layout=True)
-    fig.suptitle('Gradient test for softmax by weights', fontsize=16)
+    fig.suptitle('Gradient test by W', fontsize=16)
 
     axs[0, 0].plot(eps, grad_diff)
     axs[0, 0].set_xlabel('$\epsilon$')
@@ -120,16 +50,16 @@ def grad_test2(X_train, y_train):
     grad_diff = []
     for epss in eps:
         model_grad = copy.deepcopy(model)
-        probabilities_grad = model_grad.forward(X_train)
+        probabilities_grad = copy.deepcopy(model_grad.forward(x_data))
         model2 = copy.deepcopy(model)
         model2.graph[0].weights.data += d * epss
-        probabilities_grad2 = copy.deepcopy(model2.forward(X_train))
-        model2.backward(y_train)
+        probabilities_grad2 = copy.deepcopy(model2.forward(x_data))
+        model2.backward(x_label)
         grad_x = model2.graph[0].weights.grad
-        grad_diff.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, y_train) -
-                                utils.cross_entropy_loss(probabilities_grad, y_train) -
+        grad_diff.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, x_label) -
+                                utils.cross_entropy_loss(probabilities_grad, x_label) -
                                 epss * np.dot(d.flatten().T, grad_x.flatten())))
-    # np.dot(d.T, grad_x)))
+
     axs[1, 0].plot(eps, grad_diff)
     axs[1, 0].set_xlabel('$\epsilon$')
     axs[1, 0].set_title('$|f(x+\epsilon d) - f(x) - \epsilon d^{T} grad(x)|$')
@@ -141,24 +71,11 @@ def grad_test2(X_train, y_train):
 
     plt.show()
 
-def Jac_MV(model):
-    jacobian = model.graph[0].weights.grad
-    for layer in model.graph[1:]:
-        if layer.type == 'activation':
-            continue
-        jacobian = np.dot(jacobian, layer.weights.grad)
-
-    return jacobian
-
-def jacobian_test(X_train, y_train):
-
+def grad_test_b(X_train, y_train):
     softmax_in = 2
     softmax_out = 5
-    hidden_units = 10
     model = models.MyNeuralNetwork()
-    model.add(layers.Linear(softmax_in, hidden_units))
-    model.add(activations.Tanh())
-    model.add(layers.Softmax(hidden_units, softmax_out))
+    model.add(layers.Softmax(softmax_in, softmax_out))
     model.init()
     for p in model.parameters:
         p.grad = 0.
@@ -166,22 +83,24 @@ def jacobian_test(X_train, y_train):
     eps0 = 1
     eps = np.array([(0.5 ** i) * eps0 for i in range(10)])
 
-    one_d = np.random.randn(X_train.shape[1])
-    d = np.tile(one_d, (X_train.shape[0], 1))
-    print(one_d)
+    d = np.random.random((1, 5))
+    d = d / np.sum(d)
     grad_diff = []
 
-    for epss in eps:
-        X_train_test = X_train + epss * d
-        model_grad = copy.deepcopy(model)
-        probabilities_grad = model_grad.forward(np.array(X_train))
-        model2 = copy.deepcopy(model)
-        probabilities_grad2 = model2.forward(np.array(X_train_test))
+    x_data = np.array([X_train[0]])
+    x_label = np.array([y_train[0]])
 
-        grad_diff.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, y_train) - utils.cross_entropy_loss(probabilities_grad, y_train)))
+    for epss in eps:
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = model_grad.forward(x_data)
+        model2 = copy.deepcopy(model)
+        model2.graph[0].bias.data += d*epss
+        probabilities_grad2 = model2.forward(x_data)
+        grad_diff.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, x_label) -
+                                utils.cross_entropy_loss(probabilities_grad, x_label)))
 
     fig, axs = plt.subplots(2, 2, figsize=(12, 8), constrained_layout=True)
-    fig.suptitle('Jacobian test for softmax by weights', fontsize=16)
+    fig.suptitle('Gradient test by b', fontsize=16)
 
     axs[0, 0].plot(eps, grad_diff)
     axs[0, 0].set_xlabel('$\epsilon$')
@@ -194,22 +113,20 @@ def jacobian_test(X_train, y_train):
 
     grad_diff = []
     for epss in eps:
-        X_train_test = X_train + epss * d
         model_grad = copy.deepcopy(model)
-        probabilities_grad = model_grad.forward(np.array(X_train))
+        probabilities_grad = copy.deepcopy(model_grad.forward(x_data))
         model2 = copy.deepcopy(model)
-        probabilities_grad2 = copy.deepcopy(model2.forward(np.array(X_train_test)))
-        model2.backward(y_train)
-        jacobian = Jac_MV(model2)
-        #grad_x = model2.graph[0].weights.grad
-        grad_diff.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, y_train) -
-                                utils.cross_entropy_loss(probabilities_grad, y_train) -
-                                epss * sum(np.dot(np.array([one_d]), jacobian).T)))
-    # np.dot(d.T, grad_x)))
+        model2.graph[0].bias.data += d * epss
+        probabilities_grad2 = copy.deepcopy(model2.forward(x_data))
+        model2.backward(x_label)
+        grad_x = model2.graph[0].bias.grad
+        grad_diff.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, x_label) -
+                                utils.cross_entropy_loss(probabilities_grad, x_label) -
+                                epss * np.dot(d.flatten().T, grad_x.flatten())))
+
     axs[1, 0].plot(eps, grad_diff)
     axs[1, 0].set_xlabel('$\epsilon$')
     axs[1, 0].set_title('$|f(x+\epsilon d) - f(x) - \epsilon d^{T} grad(x)|$')
-
 
     axs[1, 1].plot(range(len(grad_diff) - 1), [grad_diff[i + 1] / grad_diff[i] for i in range(len(grad_diff) - 1)])
     axs[1, 1].set_xlabel('$i$')
@@ -218,8 +135,7 @@ def jacobian_test(X_train, y_train):
 
     plt.show()
 
-def jacobian_test2(X_train, y_train):
-
+def jacobian_test_W(X_train, y_train):
     softmax_in = 2
     softmax_out = 5
     hidden_units = 10
@@ -234,34 +150,32 @@ def jacobian_test2(X_train, y_train):
     eps0 = 1
     eps = np.array([(0.5 ** i) * eps0 for i in range(10)])
 
-    d = np.random.random((2, 5))
+    d = np.random.random((2, 10))
     d = d / np.sum(d)
 
-    d1 = np.random.random((2, 10))
-    d1 = d1 / np.sum(d1)
-
-    d2 = np.random.random((10, 5))
-    d2 = d2 / np.sum(d2)
-
+    x_data = np.array([X_train[0]])
+    x_label = np.array([y_train[0]])
 
     grad_diff = []
 
     for epss in eps:
-
         model_grad = copy.deepcopy(model)
-        probabilities_grad = model_grad.forward(np.array(X_train))
+        probabilities_grad = model_grad.forward(x_data)
         model2 = copy.deepcopy(model)
-        model2.graph[0].weights.data += d1 * epss
-        model2.graph[2].weights.data += d2 * epss
-        probabilities_grad2 = model2.forward(np.array(X_train))
-        grad_diff.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, y_train) - utils.cross_entropy_loss(probabilities_grad, y_train)))
+        model2.graph[0].weights.data += d*epss
+        probabilities_grad2 = model2.forward(x_data)
+
+        f_x_eps_d = model2.graph[1].activation_output
+        f_x = model_grad.graph[1].activation_output
+
+        grad_diff.append(LA.norm(f_x_eps_d - f_x))
 
     fig, axs = plt.subplots(2, 2, figsize=(12, 8), constrained_layout=True)
-    fig.suptitle('Jacobian test for softmax by weights', fontsize=16)
+    fig.suptitle('Jacobian test by W', fontsize=16)
 
     axs[0, 0].plot(eps, grad_diff)
     axs[0, 0].set_xlabel('$\epsilon$')
-    axs[0, 0].set_title('$|f(x+\epsilon d) - f(x)|$')
+    axs[0, 0].set_title('$||f(x+\epsilon d) - f(x)||$')
 
     axs[0, 1].plot(range(len(grad_diff) - 1), [grad_diff[i + 1] / grad_diff[i] for i in range(len(grad_diff) - 1)])
     axs[0, 1].set_xlabel('$i$')
@@ -270,23 +184,103 @@ def jacobian_test2(X_train, y_train):
 
     grad_diff = []
     for epss in eps:
+
         model_grad = copy.deepcopy(model)
-        probabilities_grad = model_grad.forward(np.array(X_train))
+        probabilities_grad = copy.deepcopy(model_grad.forward(x_data))
         model2 = copy.deepcopy(model)
-        model2.graph[0].weights.data += d1 * epss
-        model2.graph[2].weights.data += d2 * epss
-        probabilities_grad2 = copy.deepcopy(model2.forward(np.array(X_train)))
-        model2.backward(y_train)
-        jacobian = Jac_MV(model2)
-        #grad_x = model2.graph[0].weights.grad
-        grad_diff.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, y_train) -
-                                utils.cross_entropy_loss(probabilities_grad, y_train) -
-                                epss * np.dot(d.flatten().T, jacobian.flatten())))
-    # np.dot(d.T, grad_x)))
+        model2.graph[0].weights.data += d * epss
+        probabilities_grad2 = copy.deepcopy(model2.forward(x_data))
+        model_grad.backward(x_label)
+
+        f_x_eps_d = model2.graph[1].activation_output
+        f_x = model_grad.graph[1].activation_output
+
+        grad = model_grad.graph[0].weights.grad
+        JacMV = epss * np.matmul(d.T, grad)
+
+        diff = LA.norm(f_x_eps_d - f_x - JacMV)
+        grad_diff.append(diff*epss)
+
     axs[1, 0].plot(eps, grad_diff)
     axs[1, 0].set_xlabel('$\epsilon$')
-    axs[1, 0].set_title('$|f(x+\epsilon d) - f(x) - \epsilon d^{T} grad(x)|$')
+    axs[1, 0].set_title('$||f(x+\epsilon d) - f(x) -  JavMV(x, \epsilon d)||$')
 
+    axs[1, 1].plot(range(len(grad_diff) - 1), [grad_diff[i + 1] / grad_diff[i] for i in range(len(grad_diff) - 1)])
+    axs[1, 1].set_xlabel('$i$')
+    axs[1, 1].set_title('rate of decrease')
+    axs[1, 1].set_ylim([0, 1])
+
+    plt.show()
+
+def jacobian_test_b(X_train, y_train):
+    softmax_in = 2
+    softmax_out = 5
+    hidden_units = 10
+    model = models.MyNeuralNetwork()
+    model.add(layers.Linear(softmax_in, hidden_units))
+    model.add(activations.Tanh())
+    model.add(layers.Softmax(hidden_units, softmax_out))
+    model.init()
+    for p in model.parameters:
+        p.grad = 0.
+
+    eps0 = 1
+    eps = np.array([(0.5 ** i) * eps0 for i in range(10)])
+
+    d = np.random.random((1, 10))
+    d = d / np.sum(d)
+
+    x_data = np.array([X_train[0]])
+    x_label = np.array([y_train[0]])
+
+    grad_diff = []
+
+    for epss in eps:
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = model_grad.forward(x_data)
+        model2 = copy.deepcopy(model)
+        model2.graph[0].bias.data += d*epss
+        probabilities_grad2 = model2.forward(x_data)
+
+        f_x_eps_d = model2.graph[1].activation_output
+        f_x = model_grad.graph[1].activation_output
+
+        grad_diff.append(LA.norm(f_x_eps_d - f_x))
+
+    fig, axs = plt.subplots(2, 2, figsize=(12, 8), constrained_layout=True)
+    fig.suptitle('Jacobian test by b', fontsize=16)
+
+    axs[0, 0].plot(eps, grad_diff)
+    axs[0, 0].set_xlabel('$\epsilon$')
+    axs[0, 0].set_title('$||f(x+\epsilon d) - f(x)||$')
+
+    axs[0, 1].plot(range(len(grad_diff) - 1), [grad_diff[i + 1] / grad_diff[i] for i in range(len(grad_diff) - 1)])
+    axs[0, 1].set_xlabel('$i$')
+    axs[0, 1].set_title('rate of decrease')
+    axs[0, 1].set_ylim([0, 1])
+
+    grad_diff = []
+    for epss in eps:
+
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = copy.deepcopy(model_grad.forward(x_data))
+        model2 = copy.deepcopy(model)
+        model2.graph[0].bias.data += d * epss
+        probabilities_grad2 = copy.deepcopy(model2.forward(x_data))
+        model_grad.backward(x_label)
+
+        f_x_eps_d = model2.graph[1].activation_output
+        f_x = model_grad.graph[1].activation_output
+
+        grad = model_grad.graph[0].bias.grad
+        JacMV = epss * np.matmul(d.T, grad)
+
+        diff = LA.norm(f_x_eps_d - f_x - JacMV)
+        grad_diff.append(diff*epss)
+
+    axs[1, 0].plot(eps, grad_diff)
+    axs[1, 0].set_xlabel('$\epsilon$')
+    axs[1, 0].set_title('$||f(x+\epsilon d) - f(x) -  JavMV(x, \epsilon d)||$')
 
     axs[1, 1].plot(range(len(grad_diff) - 1), [grad_diff[i + 1] / grad_diff[i] for i in range(len(grad_diff) - 1)])
     axs[1, 1].set_xlabel('$i$')